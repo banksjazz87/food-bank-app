73ea620b54405ff0efbd1b8cd54e7c47450ceea6
Chris <banksjazz87@gmail.com>
2022-08-01 21:52:58 -0400

    chore: I just added a few comments on what's going on with the server

--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ var cors = require('cors');
 const app = express();
 const port = 4000;
 
+//Middleware instatiation
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//The static file that will be used on the server
 app.use('/', express.static("build"));
 
+//Just a console.log statement to let you know that the server is up and running and the port number.
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
 
