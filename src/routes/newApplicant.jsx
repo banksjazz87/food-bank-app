4b5fa102519df9ab07a1d357ac2d731c5c1a97f4
Chris <banksjazz87@gmail.com>
2022-08-09 06:35:04 -0400

    chore: I have created a data set for the different inputs that will be needed for a new applicant

--- src/routes/newApplicant.jsx
+++ src/routes/newApplicant.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 export default function Applicant() {
+  const dataPoints = [
+      {initialValue: "", type:"text", maxWidth: 25, name: "firstName"}, 
+      {initialValue: "", type: "text", maxWidth: 25, name: "lastName"}, 
+      {initialValue: "", type: "tel", maxWidth: 15, name: "phone"},
+      {initialValue: "", type: "text", maxWidth: 25, name: "address"}, 
+      {initialValue: 0, type: "number", maxWidth: 2, name: "Number of People In Household"}, 
+      {initialValue: 0, type: "number", maxWidth: 2, name: "Number of Children"}, 
+      {initialValue: 0, type: "number", maxWidth: 2, name: "Number of Adults"}, 
+      {initialValue: 0, type: "number", maxWidth: 2, name: "Number of Citizens"}, 
+      {initialValue: null, type: null, maxWidth: null, name: "Total Household Income"}, 
+      {initialValue: 0, type: "number", maxWidth: 6, name: "Monthly Income"}, 
+      {initialValue: 0, type: "number", maxWidth: 6, name: "Anual Income"}, 
+      {initialValue: 0, type: "number", maxWidth: 6, name: "Weekly Income"}
+    ];
     return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>New Applicant Page</h2>
-      </main>
+      <div id="new_applicat_wrapper">
+
+      </div>
     );
   }
