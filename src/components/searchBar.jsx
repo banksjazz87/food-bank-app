8aca030afe4763f0bbc99c0a45a1df8c72c60eb9
Chris <banksjazz87@gmail.com>
2022-09-13 06:31:56 -0400

    chore: I am working on creating a method to store the information needed when the user selects a name from the list of applicants

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -3,7 +3,9 @@ import React, { useState, useEffect } from "react";
 //This component creates a search bar for all of the applicants currently in the database.
 export default function AllApplicantSearchBar() {
 
+    //Setting the initial applicant data.
     const [data, setData] = useState([]);
+    const [input, setInput] = useState({});
 
     useEffect(() => {
         fetch("/all-applicants")
@@ -11,6 +13,19 @@ export default function AllApplicantSearchBar() {
         .then((final) => {setData(final);})
         .catch((e) => console.log('error', e));
     }, []);
+
+    const returnNums = (string) => {
+        let num = "";
+        
+        for (let i = 0; i < string.length; i++) {
+            let numCheck = string[i].parseInt();
+            if (numCheck.isNaN()) {
+                num = num + string[i];
+            }
+        }
+
+        return num;
+    }
 
     const allNames = (array) => {
         const renderOptions = array.map((x, y) => {
@@ -35,7 +50,10 @@ export default function AllApplicantSearchBar() {
         return (
             <div>
                 <label for="applicants">Applicants</label>
-                <select name="applicants" id="applicants">
+                <select 
+                    name="applicants" 
+                    id="applicants"
+                    onChange={(e) => console.log(returnNums(e.target.value))}>
                     {allNames(data)}
                 </select>
             </div>
