a0fac14212c2fc1644fbf1002a110927d96b430f
Chris <banksjazz87@gmail.com>
2022-09-15 21:31:02 -0400

    chore: I have all of the required information needed from the search bar component in place to make a get request for specific applicant information

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -2,61 +2,90 @@ import React, { useState, useEffect } from "react";
 
 //This component creates a search bar for all of the applicants currently in the database.
 export default function AllApplicantSearchBar() {
+  //Setting the initial applicant data.
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState({});
 
-    //Setting the initial applicant data.
-    const [data, setData] = useState([]);
-    const [input, setInput] = useState({});
+  useEffect(() => {
+    fetch("/all-applicants")
+      .then((response) => response.json())
+      .then((final) => {
+        setData(final);
+      })
+      .catch((e) => console.log("error", e));
+  }, []);
 
-    useEffect(() => {
-        fetch("/all-applicants")
-        .then((response) => response.json())
-        .then((final) => {setData(final);})
-        .catch((e) => console.log('error', e));
-    }, []);
+  const returnNums = (string) => {
+    let num = "";
 
-    const returnNums = (string) => {
-        let num = "";
-        
-        for (let i = 0; i < string.length; i++) {
-            let numCheck = parseInt(string[i]);
-            if (!isNaN(numCheck)) {
-                num = num + string[i];
-            }
-        }
-
-        return num;
+    for (let i = 0; i < string.length; i++) {
+      let numCheck = parseInt(string[i]);
+      if (!isNaN(numCheck)) {
+        num = num + string[i];
+      }
     }
 
-    const allNames = (array) => {
-        const renderOptions = array.map((x, y) => {
-            return (
-                <option key={`option_${y}`} id={`option_${array[y].ApplicantID}`}>{`${y+1}. ${array[y].firstName} ${array[y].lastName}`}</option>
-            )
-        });
+    return num;
+  };
 
-        return renderOptions;
-    }
+  const selectedItem = (index, array) => {
+    const itemIndex = index - 1;
+    setInput((input) => ({
+      ...input,
+      key: array[itemIndex].ApplicantID,
+      firstName: array[itemIndex].firstName,
+      lastName: array[itemIndex].lastName,
+    }));
+  };
 
-    if (data.length === 0) {
-        return (
-            <div>
-            <label for="applicants">Applicants</label>
-            <select name="applicants" id="applicants">
-                <option id="fetching">Fetching...</option>
-            </select>
-            </div>
-        )
-    } else {
-        return (
-            <div>
-                <label for="applicants">Applicants</label>
-                <select 
-                    name="applicants" 
-                    id="applicants"
-                    onChange={(e) => console.log(returnNums(e.target.value))}>
-                    {allNames(data)}
-                </select>
-            </div>
-        )
-    }
+  const allNames = (array) => {
+    const renderOptions = array.map((x, y) => {
+      return (
+        <option key={`option_${y}`} id={`option_${array[y].ApplicantID}`}>{`${
+          y + 1
+        }. ${array[y].firstName} ${array[y].lastName}`}</option>
+      );
+    });
+
+    return renderOptions;
+  };
+
+  if (data.length === 0) {
+    return (
+      <div>
+        <label for="applicants">Applicants</label>
+        <select name="applicants" id="applicants">
+          <option id="fetching">Fetching...</option>
+        </select>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <form
+          id="applicantSearch"
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(input);
+          }}
+        >
+          <label for="applicants">Applicants</label>
+          <select
+            name="applicants"
+            id="applicants"
+            onChange={(e) => {
+              selectedItem(returnNums(e.target.value), data);
+            }}
+          >
+            {allNames(data)}
+          </select>
+          <input 
+            id="applicantSearchSubmit" 
+            type="submit" 
+            value="submit"
+            />
+        </form>
+      </div>
+    );
+  }
 }
