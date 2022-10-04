83e94ce8d659b3374931af9d00fdf053b2504ca1
Chris <banksjazz87@gmail.com>
2022-10-03 21:12:51 -0400

    chore: I am working on creating some more styling on the navigation page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './assets/styles/App.scss';
 import './assets/styles/root.scss';
 import './assets/styles/library.scss';
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import HamburgerIcon from "./components/mobileHamburger.jsx";
 
 export default function App() {
+
+  const [displayNav, setNavDisplay] = useState(false);
+  //const [showMobileNav, setMobileNav] = useState(true);
+
+  const showNavBar = () => {
+    if (displayNav) {
+      setNavDisplay(false);
+    } else {
+      setNavDisplay(true);
+    }
+  }
+
   return (
     <div id="homepage_wrapper">
      <div id="header_wrapper">
       <h1>Food Bank Application</h1>
       </div>
-      <nav
+      <nav 
+        id="nav_wrapper"
+        style={displayNav ? {width: "30%", opacity: "1"} :  {width: "0", opacity: "0"}}
       >
         <Link to="/login">Login</Link> 
         <Link to="/new_applicant">New Applicant</Link>
@@ -19,7 +33,7 @@ export default function App() {
         <Link to="/current_registered_list">Food Bank List</Link>
         <Link to="/past_registered_list">Past Registered List</Link>
       </nav>
-      <HamburgerIcon />
+      <HamburgerIcon clickHandler={showNavBar} />
     </div>
   );
 }
--- src/components/mobileHamburger.jsx
+++ src/components/mobileHamburger.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "../assets/styles/hamburger.scss";
 
-export default function HamburgerIcon() {
+export default function HamburgerIcon(props) {
   return (
-    <div id="hamburg_wrapper">
+    <div 
+      id="hamburg_wrapper"
+      onClick={props.clickHandler}>
       <div class="hamburger_line"></div>
       <div class="hamburger_line"></div>
       <div class="hamburger_line"></div>
