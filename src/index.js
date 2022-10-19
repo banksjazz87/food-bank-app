613a656660de703b84a0545d1fc81fd3b4b3f0af
Chris <banksjazz87@gmail.com>
2022-10-18 21:40:44 -0400

    chore: I have created a file that will be used for creating, reading, updating and deleting lists

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/App.scss';
 import './assets/styles/index.scss';
-import './assets/styles/foodBankList.scss';
+import './assets/styles/displayFoodBankList.scss';
 import './assets/styles/login.scss';
 import './assets/styles/newApplicant.scss';
 import './assets/styles/pastLists.scss';
@@ -17,9 +17,10 @@ import App from "./App";
 import Applicant from "./views/newApplicant.jsx";
 import Login from "./views/login.jsx";
 import SearchApplicants from "./views/searchApplicants.jsx";
-import FoodBankList from "./views/foodBankList.jsx";
+import DisplayFoodBankList from "./views/displayFoodBankList.jsx";
 import PastLists from "./views/pastLists.jsx";
 import Dashboard from "./views/dashBoard.jsx";
+import FoodBankList from "./views/foodBankList.jsx";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -31,7 +32,9 @@ root.render(
       <Route path="new_applicant" element={<Applicant />} />
       <Route path="login" element={<Login />} />
       <Route path="search" element={<SearchApplicants />} />
-      <Route path="current_registered_list" element={<FoodBankList />} />
+      <Route path="current_registered_list" element={<DisplayFoodBankList />} />
+      <Route path="foodbank-lists" element={<FoodBankList />} />
+
       <Route path="past_registered_list" element={<PastLists />} />
     </Routes>
   </BrowserRouter>
