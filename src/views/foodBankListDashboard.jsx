5f313ef87998f85d5f5e4665d21737f7a5f71043
Chris <banksjazz87@gmail.com>
2022-10-23 07:09:29 -0400

    chore: I have corrected an error that was preventing the fb list dashboard page from being displayed

--- src/views/foodBankListDashboard.jsx
+++ src/views/foodBankListDashboard.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import FBDashboardIcon from "../components/dashboardIcon.jsx";
 import NavBar from "../components/navBar.jsx";
+
+import CreateFbList from "../components/createFbList.jsx";
 import "../assets/styles/fbListDashboard.scss";
 
+
+
 export default function FoodBankListDashboard() {
+
+const [showCreate, setShowCreate] = useState(false);
+
   return (
     <div id="fb_list_dashboard">
+
       <h1>FB List Dashboard</h1>
       <NavBar />
-      <div id="icon_wrapper">
-        <FBDashboardIcon title="Create List" />
+
+    <div id="icon_wrapper">
+      <CreateFbList
+       clickHandler={() => setShowCreate(true)}
+       showForm={showCreate}
+       />
+        
+
+        <div id="search_past_lists">
         <FBDashboardIcon title="Search Lists" />
+        </div>
       </div>
     </div>
   );
