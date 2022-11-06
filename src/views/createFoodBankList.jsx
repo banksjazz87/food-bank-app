39893f688623428f7cdeab32107f8df960d2d570
Chris <banksjazz87@gmail.com>
2022-11-05 21:12:54 -0400

    chore: I have created a few methods to hide certain elements when they aren't needed

--- src/views/createFoodBankList.jsx
+++ src/views/createFoodBankList.jsx
@@ -4,21 +4,23 @@ import postRequest from "../functions/post.js";
 import AllApplicantSearchBar from "../components/searchBar.jsx";
 import deleteRequest from "../functions/deleteRequest.js";
 
-export default function CreateFoodBankList(props) {
+export default function CreateFoodBankList() {
   const [listName, setListName] = useState({ title: "" });
   const [listData, setListData] = useState({
     title: "",
     attendants: [],
     firstLast: [],
   });
-  const [displaySaveEdit, setDisplaySaveEdit] = useState(false);
+  const [displayEdit, setDisplayEdit] = useState(false);
   const [editMode, setEditMode] = useState(false);
+  const [showApplicantBar, setShowApplicantBar] = useState(false);
 
   //Function that is used when the submit button is pushed after creating the title.
   const submitListTitle = () => {
     postRequest("/new_foodbank_list", listName).then((data) => {
       if (data.message === "success") {
         setListData({ ...listData, title: data.title });
+        setShowApplicantBar(true);
       } else {
         alert(data.message);
       }
@@ -26,19 +28,28 @@ export default function CreateFoodBankList(props) {
   };
 
   //Function to display the save and edit buttons.
-  const showSaveEdit = () => {
-    if (!displaySaveEdit) {
-      setDisplaySaveEdit(true);
-    }
+  const showEdit = () => {
+    if (!displayEdit) {
+      setDisplayEdit(true);
+    } 
+  };
+
+  //Function to save the list in the database.
+  const saveList = (table, obj) => {
+    postRequest(`/save-list/list-name/${table}`, obj).then((data) => {
+      if (data.message !== "success") {
+        alert(data.message);
+      }
+    });
   };
 
   /**
    *
    * @param {*} array
-   * @description this function will check and make sure that their isn't already an attendant with the same first and last name in the list.  If there is a duplicate, an alert will be displayed notifying the user.  Otherwise, the name will be added to the list and all of the attendant data will be stored in the listData.attendants array.  The showSaveEdit function is also called to determine if the save and edit buttons should be displayed.
+   * @description this function will check and make sure that their isn't already an attendant with the same first and last name in the list.  If there is a duplicate, an alert will be displayed notifying the user.  Otherwise, the name will be added to the list and all of the attendant data will be stored in the listData.attendants array.  The showEdit function is also called to determine if the save and edit buttons should be displayed.
    */
   const addNewAttendant = (array) => {
-    showSaveEdit();
+    showEdit();
 
     let previousFirstLast = listData.firstLast.slice();
     let currentFirstLast = `${array[0].firstName}${array[0].lastName}`;
@@ -51,7 +62,6 @@ export default function CreateFoodBankList(props) {
         firstLast: previousFirstLast.concat([currentFirstLast]),
       });
       saveList(listData.title, array[0]);
-      
     } else {
       alert(
         `${array[0].firstName} ${array[0].lastName} is already included in the current list.`
@@ -59,32 +69,21 @@ export default function CreateFoodBankList(props) {
     }
   };
 
-  
-
-  //Function to save the list in the database.
-  const saveList = (table, obj) => {
-    postRequest(`/save-list/list-name/${table}`, obj).then(
-      (data) => {
-        if (data.message !== "success") {
-        alert(data.message);
-        }
-      }
-    );
-  };
-
-  //function to handle the delete event 
+  //function to handle the delete event
   const deleteAttendantFromList = (index) => {
     const previousAttendants = listData.attendants.slice();
     previousAttendants.splice(parseInt(index), 1);
     setListData({ ...listData, attendants: previousAttendants });
-  }
+  };
 
   //Remove attendant from the database
-  const deleteAttendantFromDatabase = (array,index, table) => {
+  const deleteAttendantFromDatabase = (array, index, table) => {
     const indexOfSelected = parseInt(index);
-    deleteRequest(`/remove-attendant/table/${table}`, array[indexOfSelected]).then(data => console.log(data));
-
-  }
+    deleteRequest(
+      `/remove-attendant/table/${table}`,
+      array[indexOfSelected]
+    ).then((data) => console.log(data));
+  };
 
   const displayApplicants = (array) => {
     const layOutApplicants = array.map((x, y) => {
@@ -96,9 +95,13 @@ export default function CreateFoodBankList(props) {
             style={editMode ? { display: "" } : { display: "none" }}
             onClick={(e) => {
               deleteAttendantFromList(e.target.id);
-              deleteAttendantFromDatabase(listData.attendants, e.target.id, listData.title);
-             }
-            }
+              deleteAttendantFromDatabase(
+                listData.attendants,
+                e.target.id,
+                listData.title
+              );
+              setEditMode(false);
+            }}
           >
             Delete
           </button>
@@ -135,20 +138,25 @@ export default function CreateFoodBankList(props) {
         />
         <input type="submit" value="Submit" />
       </form>
-      <AllApplicantSearchBar
-        handleChange={addNewAttendant}
-        value="Add To List"
-      />
-      <h1>{listData.title}</h1>
-      {displayApplicants(listData.attendants)}
-      <button
-        class="edit_button"
-        type="button"
-        style={displaySaveEdit ? { display: "" } : { display: "none" }}
-        onClick={() => setEditMode(true)}
+      <div
+        id="applicant_search_and_list"
+        style={showApplicantBar ? { display: "" } : { display: "none" }}
       >
-        Edit
-      </button>
+        <AllApplicantSearchBar
+          handleChange={addNewAttendant}
+          value="Add To List"
+        />
+        <h1>{listData.title}</h1>
+        {displayApplicants(listData.attendants)}
+        <button
+          class="edit_button"
+          type="button"
+          style={displayEdit ? { display: "" } : { display: "none" }}
+          onClick={() => setEditMode(true)}
+        >
+          Edit
+        </button>
+      </div>
     </div>
   );
 }
