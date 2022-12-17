8935403bad5fde4f203861f35ef1279066d581e9
Chris <banksjazz87@gmail.com>
2022-12-17 13:33:12 -0500

    chore: I have added a few styling corrections

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -60,18 +60,12 @@ export default function NavBar() {
   if (fullNav) {
     return (
       <>
-        <nav
-          className={
-            displayNav === "standard" ? "nav_wrapper" : "mobile_nav_wrapper"
-          }
-        >
+        <nav className={displayNav === "standard" ? "nav_wrapper" : "mobile_nav_wrapper"}>
           <Link to="/dashboard">Dashboard</Link>
-          <Link to="/login">Login</Link>
           <Link to="/new_applicant">New Applicant</Link>
           <Link to="/search">Search Applicants</Link>
           <Link to="/foodbank-list-dashboard">Food Bank List Dashboard</Link>
         </nav>
-
         <HamburgerIcon clickHandler={showNavBar} />
       </>
     );
@@ -79,14 +73,9 @@ export default function NavBar() {
   } else {
     return (
       <>
-        <nav
-          className={
-            displayNav === "standard" ? "nav_wrapper" : "mobile_nav_wrapper"
-          }
-        >
+        <nav className={displayNav === "standard" ? "nav_wrapper" : "mobile_nav_wrapper"}>
           <Link to="/login">Login</Link>
         </nav>
-
         <HamburgerIcon clickHandler={showNavBar} />
       </>
     );
