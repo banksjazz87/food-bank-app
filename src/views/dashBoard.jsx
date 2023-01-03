0cc64a88a21ac3e168f330a083b8a28fce742819
Chris <banksjazz87@gmail.com>
2023-01-03 17:03:56 -0500

    chore: I have worked on a bit more styling on the dashboard page

--- src/views/dashBoard.jsx
+++ src/views/dashBoard.jsx
@@ -17,17 +17,35 @@ export default function Dashboard() {
         <h1>Dashboard</h1>
       </div>
       <NavBar />
-
+    <div id="content_wrapper">
       <h1>Applicants</h1>
+      <div class="icon_wrapper">
         <DashboardIcon 
           clickHandler={() => navigate('/new_applicant', {replace: true})}
           title="New Applicant"
         />
         <DashboardIcon
           clickHandler={() => navigate('/search', {replace: true})}
-          title="Search and Edit"
+          title="Search Edit"
         />
+      </div>
       <h1>Foodbank Lists</h1>
+      <div class="icon_wrapper">
+        <DashboardIcon 
+          clickHandler={() => navigate('/create-foodbank-list', {replace: true})}
+          title="Create List"
+        />
+        <DashboardIcon
+          clickHandler={() => navigate('/past-rigestered-list', {replace: true})}
+          title="Past Lists"
+        />
+
+        <DashboardIcon
+          clickHandler={() => navigate('/current-rigestered-list', {replace: true})}
+          title="Current List"
+        />
+        </div>
+        </div>
     </div>
   );
 }
