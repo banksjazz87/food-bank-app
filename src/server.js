ed0b97e41baec4411fb65fc3c77be2820025a198
Chris Banks <banksjazz87@gmail.com>
2023-02-08 15:24:14 -0500

    chore: I have created a database file and saved it to the gitignore and I have removed the test.js file

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 var cors = require("cors");
 const app = express();
 const port = 4000;
@@ -10,10 +11,15 @@ const Dummy = require("./variables/dummyData.js");
 //Middleware instantiation
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.urlencoded({ extended: false }));
 
 //The static file that will be used on the server
-app.use("/", express.static("build"));
+//app.use("/", express.static("build"));
+app.use(express.static('/home/cbanks87/Documents/foodBankApp/my-app/build'))
+
+app.get('/', function (req, res) {
+  res.sendFile('/home/cbanks87/Documents/foodBankApp/my-app/build');
+})
 
 //Just a console.log statement to let you know that the server is up and running and the port number.
 app.listen(port, () => {
