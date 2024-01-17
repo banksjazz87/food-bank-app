e8b35ad3eef664e5ad77379a13a050ee46006cf5
Chris <banksjazz87@gmail.com>
2024-01-17 08:26:02 -0500

    feature: updated the editing bar to be present at all times.

--- src/components/attendanceSubHeading.jsx
+++ src/components/attendanceSubHeading.jsx
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from "react";
 import "../assets/styles/attendanceSubHeading.scss";
 
 export default function AttendanceSubHeading(props) {
-	const [stickyHeader, setSticky] = useState(false);
+	const [stickyHeader, setSticky] = useState(true);
 	const [currentView, setCurrentView] = useState("Check In");
 
 	useEffect(() => {
-		const mainHeader = document.getElementById("current_fb_list_header_wrapper");
-		const headerLocation = mainHeader.getBoundingClientRect().height;
+	    const mainHeader = document.getElementById("current_fb_list_header_wrapper");
+	    const headerLocation = mainHeader.getBoundingClientRect().height;
 
-		document.addEventListener("scroll", () => {
-			let currentLocation = window.scrollY;
-			if (currentLocation > headerLocation) {
-				setSticky(true);
-			} else {
-				setSticky(false);
-			}
-		});
+	    document.addEventListener("scroll", () => {
+	        let currentLocation = window.scrollY;
+	        if (currentLocation > headerLocation) {
+	            setSticky(true);
+	        } else {
+	            setSticky(false);
+	        }
+	    });
 	});
 
 	useEffect(() => {
@@ -38,18 +38,18 @@ export default function AttendanceSubHeading(props) {
 	});
 
 	return (
-		<div 
-            id="attendance_sub_heading_wrapper" 
-            className={stickyHeader ? "sticky_heading" : "non_sticky_header"}
-        >
-			<div
-				id="attendance_sub_heading"
-			>
-				<p>{props.currentCount}</p>
+		<div
+			id="attendance_sub_heading_wrapper"
+			className={stickyHeader ? "sticky_heading" : "non_sticky_header"}
+		>
+			<div id="attendance_sub_heading">
+				<p> {props.currentCount} </p>
 			</div>
-            <div id="current_view_heading" style={stickyHeader ? {display: ""} : {display: "none"}}>
-                <p>{`Currently Editing ${currentView}`}</p>
-            </div>
+			<div
+				id="current_view_heading"
+			>
+				<p> {`Currently Editing ${currentView}`} </p>
+			</div>
 		</div>
 	);
 }
